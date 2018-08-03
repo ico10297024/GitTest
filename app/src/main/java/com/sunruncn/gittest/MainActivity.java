421be9b457d4578598f4a7ca4675aa68478858c6
ico <ico10297024@vip.qq.com>
2018-08-03 16:41:13 +0800

    Merge branch 'master' of https://github.com/ico10297024/GitTest
    # Conflicts:
    #	.gitignore

--- app/src/main/java/com/sunruncn/gittest/MainActivity.java
+++ app/src/main/java/com/sunruncn/gittest/MainActivity.java
@@ -2,12 +2,16 @@ package com.sunruncn.gittest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ico";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(TAG, "onCreate: ");
     }
 }
